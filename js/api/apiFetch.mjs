dc8318e353048223b3e0b1952430bf73e739d4bb
Martin Mroz <mamr@hotmail.no>
2022-11-21 00:34:36 +0100

    re-sized images

---
+++ js/api/apiFetch.mjs
@@ -0,0 +1,4 @@
+const API_HOST_URL = "https://nf-api.onrender.com";
+const API_BASE = "/api/v1";
+const API_AUCTION_BASE = "/auction";
+const API_AUCTION_URL = `${API_HOST_URL}${API_BASE}${API_AUCTION_BASE}`;
