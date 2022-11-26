d2356fb8b24a26666b9ca203e61f43b1127d5dbf
Martin Mroz <mamr@hotmail.no>
2022-11-26 17:30:24 +0100

    listings coming through on page

--- js/listings/read.mjs
+++ js/listings/read.mjs
@@ -1,17 +1,70 @@
 import { API_AUCTION_URL } from "../api/apiFetch.mjs";
 
 const action = "/listings";
+const listingsCard = document.querySelector(".recentListings")
 
 export async function getListings() {
-  const updatePostURL = `${API_AUCTION_URL}${action}`;
-   
+  const updatePostURL = `${API_AUCTION_URL}${action}`;  
   const response = await fetch(updatePostURL)
+  const listings = await response.json();
 
-  return await response.json();
+  console.log(listings)
+
+  for (let i = 0; i < listings.length; i++) {
+    listingsCard.innerHTML +=
+  `<a href="details.html?id=${listings[i].id}">
+  <div class="card lg:card-side bg-base-100 shadow-xl">
+  <figure><img src="${listings[i].media}" alt="Album"/></figure>
+  <div class="card-body">
+  <h2 class="card-title">${listings[i].title}</h2>
+  <p>${listings[i].tags}</p>
+  <div class="card-actions justify-end">
+  <button class="btn btn-primary">Bid</button>
+  <li>description: ${listings[i].description}</li>
+</div>
+</div>
+</div>
+  </div>
+  </a>`;
+  }
+
+  } 
+
+ 
+
+/*<div class="card lg:card-side bg-base-100 shadow-xl">
+  <figure><img src="https://placeimg.com/400/400/arch" alt="Album"/></figure>
+  <div class="card-body">
+    <h2 class="card-title">New album is released!</h2>
+    <p>Click the button to listen on Spotiwhy app.</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Listen</button>
+    </div>
+  </div>
+</div> */
+
+/*
+for (let i = 0; i < listings.length; i++) {
+  listingsCard.innerHTML +=
+`<a href="details.html?id=${listings[i].id}">
+<h2>${listings[i].title}</h2>
+<ul>
+<li>description: ${listings[i].description}</li>
+<li>Img: ${listings[i].media}</li>
+<li>Tags: ${listings[i].tags}</li>
+
+</ul>
+</div>
+</a>`;
 }
+
+*/
 
 getListings()
 console.log(getListings())
+
+/*
+
 
 export async function getListing(id) {
   if (!id) {
@@ -27,3 +80,6 @@ export async function getListing(id) {
 
 getListing()
 console.log(getListing())
+
+
+*/
