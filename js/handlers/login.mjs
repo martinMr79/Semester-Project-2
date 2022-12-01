bac391f4742fc284954d0017f1a1ce81cdd760d2
Martin Mroz <mamr@hotmail.no>
2022-12-01 20:47:05 +0100

    login working

--- js/handlers/login.mjs
+++ js/handlers/login.mjs
@@ -1,24 +1,29 @@
-/*import { login } from "/js/api/auth/login.mjs";
+import { login } from "../api/auth/login.mjs";
 
 /**
  * Stores user input as an object when form is submitted
  */
 
-/*
+
  export async function setLoginFormListener() {
   const form = document.querySelector("#loginForm");
 
-  if (form) {
-    form.addEventListener("submit", handleSubmit)
-  }
+
+    form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const form = event.target;
+    const formData = new FormData(form);
+    const profile = Object.fromEntries(formData.entries())
+
+      // send it to the API 
+      login(profile)
+
+  })
+
 }
 
-async function handleSubmit(evt) {
+/*
 
-  evt.preventDefault()
-  const form = evt.target;
-  const formData = new FormData(form);
-  const profile = Object.fromEntries(formData.entries())
   
   const email = profile["email"];
   const password = profile["password"];
