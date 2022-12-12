f5c5ecbbdfd508fdb2f913a4315b4ae0cbe5b357
Martin Mroz <mamr@hotmail.no>
2022-12-12 17:23:07 +0100

    error fix

--- js/profile/profileData.mjs
+++ js/profile/profileData.mjs
@@ -7,16 +7,16 @@ const action = "/profiles";
 const author = "?_seller=true&_count=true&_bids=true&_listings=true";
 
 console.log(load("profile"));
-console.log(load("profile"));
+
 
 export async function readProfile (name) {
 
 
     if(!name){
-      throw new Error("Requires a name");
+      throw new Error("Login to see user details");
     }
   
-    const profileURL = `${API_AUCTION_URL}${action}/${name}${author}`;
+    const profileURL = `${API_AUCTION_URL}${action}/${name}${author} `;
   
     const response = await authFetch (profileURL)
   
@@ -34,17 +34,18 @@ export async function profileInfo() {
     const userName = document.querySelector("#userName");
     const userAvatar = document.querySelector(".userAvatar");
     const userCredits = document.querySelector(".credits");
-    const userListings = document.querySelector(".nrListings");
-    const userBids = document.querySelector(".userBids");
-    const listingsContainer = document.querySelector(".listings");
     
     const { name, credits } = load("profile");
     const avatar = load("avatar");
     
+    
+
     //User information 
     userName.innerText = name; 
     userAvatar.src = avatar; 
     userCredits.innerText = credits; 
+
+    console.log(userAvatar)    
 
  }
 
