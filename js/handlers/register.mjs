6aaed39f5f08e2217a34241dcff0fc4eba7fc741
Martin Mroz <mamr@hotmail.no>
2022-12-15 21:07:12 +0100

    remove unused code

--- js/handlers/register.mjs
+++ js/handlers/register.mjs
@@ -22,23 +22,3 @@ export function setRegisterFormListener() {
 }
 
 
-/*
-  async function handleSubmit(evt) {
-  evt.preventDefault()
-  const form = evt.target;
-  const formData = new FormData(form);
-  const profile = Object.fromEntries(formData.entries()) 
-
-  
-
-  const name = profile["name"]
-  const email = profile["email"];
-  const password = profile["password"];
-  if (email && password) {
-    const result = await register(name, email, password)
-    location.href = "/public/index.html";
-   
-  }
-  console.log(profile)
-}
-*/
