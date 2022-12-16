91e5e758ffa9a5512d43136fcb82f69f77c0f0ce
Martin Mroz <mamr@hotmail.no>
2022-12-16 09:55:37 +0100

    dynamic title

--- js/listings/details.mjs
+++ js/listings/details.mjs
@@ -14,22 +14,31 @@ const listings = await response.json();
 
 console.log(listings);
 
+const listingTitle = document.querySelector(".listingTitle")
+listingTitle.innerHTML += 
+`<title>${listings.title}</title>
+<h1 class="card-title text-center mb-8">${listings.title}</h1>`
+
 const listingDetails = document.querySelector(".listingDetails")
 listingDetails.innerHTML += 
-`  <div class= "card lg:card-side bg-base-200 shadow-xl  max-h-sm">
-<figure ><img class="rounded-full" src="${listings.media}" alt="Album"/></figure>
+`  <div class= "card lg:card-side bg-base-200 shadow-xl max-w-sm max-h-sm">
+<figure class="w-full max-h-42"><img class="rounded" src="${listings.media}" alt="Album"/></figure>
 <div class="card-body">
-<h1 class="card-title">${listings.title}</h1>
 <p>${listings.tags}</p>
 <div class="card-actions justify-start">
 <p>bids: ${listings._count.bids}</p>
 <p>Current bid:</p>
-<p>Description: ${listings.description}</p>`
+<p>Description: ${listings.description}</p>
+<button class="btn btn-success">Bid now</button>
+</div>
+</div>
+</div>
+
+<div>dfdfdf</div>
+`
 
 
-const listingTitle = document.querySelector(".listingTitle")
-listingTitle.innerHTML += 
-`<h2 class="card-title">${listings.title}</h2>`
+
 
 
 
