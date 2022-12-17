e7ae57f038110a1f755e3826557f556e85931df1
Martin Mroz <mamr@hotmail.no>
2022-12-17 04:30:09 +0100

    added media placeholder img

--- js/listings/details.mjs
+++ js/listings/details.mjs
@@ -22,6 +22,11 @@ listingTitle.innerHTML +=
 `<title>${listings.title}</title>
 <h1 class="card-title text-center mb-8">${listings.title}</h1>`
 
+if (listings.media.length === 0) {
+  listings.media = 'https://www.freeiconspng.com/uploads/no-image-icon-4.png" width="350" alt="Simple No Png';
+}
+
+
 const listingDetails = document.querySelector(".listingDetails")
 listingDetails.innerHTML += 
 `<div class= "flex flex-row">  
@@ -45,34 +50,8 @@ listingDetails.innerHTML +=
 </div>
 `
 
-/*
-export const buildBidInfo = (data) => {
-
-let body;
-let amount = 0;
-
-if (data.bids.length === 0) {
-  body = errorMessage("No bids yet");
-} else {
-  const bidders = sortAmountAsc(data.bids);
-  const dateFix = timeAgo(bidders[0].created);
-
-  amount = bidders[0].amount;
-
-  body = `
-    <div class="flex flex-col gap-2 w-full">
-      <h3 class="text-xl">Current bid</h3>
-      <h4 class="text-lg font-semibold">${bidders[0].bidderName}</h3>
-      <p>Amount: ${bidders[0].amount}</p>
-      <p>${dateFix}</p>
-    </div>`;
-
 }
-*/
-
-} 
   
-/*buildBidInfo()*/
   getListing()
   
 
--- js/listings/read.mjs
+++ js/listings/read.mjs
@@ -1,6 +1,6 @@
 import { API_AUCTION_URL } from "../api/apiURL.mjs";
 
-const action = "/listings?_bids=true&limit=16";
+const action = "/listings?_bids=true&limit=50";
 const listingsCard = document.querySelector(".recentListings")
 
 export async function getListings() {
@@ -11,8 +11,14 @@ export async function getListings() {
   console.log(listings)
 
   for (let i = 0; i < listings.length; i++) {
+
+    if (listings[i].media.length === 0) {
+      listings[i].media = 'https://www.freeiconspng.com/uploads/no-image-icon-4.png" width="100" alt="Simple No Png';
+    }
+    
+
     listingsCard.innerHTML +=
-  
+
   `<a href="details.html?id=${listings[i].id}" class="">
   <div class="card bg-slate-200 shadow-xl max-h-72">
   <figure class="bg-img w-full max-h-62"><img class="rounded-lg" src="${listings[i].media}" alt="Album"/></figure>
@@ -24,24 +30,12 @@ export async function getListings() {
   <p>Description: ${listings[i].description}</p>
   </div>
 </div>
-  </a>`;
+  </a>`; 
 
 }
+
 }
   getListings()
 
-  /*
-  if(listings[i].bids.length == 0){
-    listingsCard.innerHTML +=
-    `<p>Current bid: </p>`
-  } else {
-    listingsCard.innerHTML +=
-    `<p>Current bid: ${listings[i].bids.reverse()[0].amount}</p>`
-  }
-  listingsCard.innerHTML +=
-  `<p>bids: ${listings[i]._count.bids}</p>
-;
-  }
 
-  } */
 
