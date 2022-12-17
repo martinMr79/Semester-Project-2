19c59db558ff6e5f83a90b665c2d0fd1c3da5047
Martin Mroz <mamr@hotmail.no>
2022-12-17 04:54:13 +0100

    placeholder img added

--- js/listings/read.mjs
+++ js/listings/read.mjs
@@ -1,25 +1,22 @@
 import { API_AUCTION_URL } from "../api/apiURL.mjs";
 
-const action = "/listings?_bids=true&limit=50";
-const listingsCard = document.querySelector(".recentListings")
+const action = "/listings?_bids=true&limit=20";
+const listingsCard = document.querySelector(".recentListings");
 
 export async function getListings() {
-  const updatePostURL = `${API_AUCTION_URL}${action}`;  
-  const response = await fetch(updatePostURL)
+  const updatePostURL = `${API_AUCTION_URL}${action}`;
+  const response = await fetch(updatePostURL);
   const listings = await response.json();
 
-  console.log(listings)
+  console.log(listings);
 
   for (let i = 0; i < listings.length; i++) {
-
     if (listings[i].media.length === 0) {
-      listings[i].media = 'https://www.freeiconspng.com/uploads/no-image-icon-4.png" width="100" alt="Simple No Png';
+      listings[i].media =
+        'https://www.freeiconspng.com/uploads/no-image-icon-4.png" width="100" alt="Simple No Png';
     }
-    
 
-    listingsCard.innerHTML +=
-
-  `<a href="details.html?id=${listings[i].id}" class="">
+    listingsCard.innerHTML += `<a href="details.html?id=${listings[i].id}" class="">
   <div class="card bg-slate-200 shadow-xl max-h-72">
   <figure class="bg-img w-full max-h-62"><img class="rounded-lg" src="${listings[i].media}" alt="Album"/></figure>
   <div class="card-body">
@@ -30,12 +27,7 @@ export async function getListings() {
   <p>Description: ${listings[i].description}</p>
   </div>
 </div>
-  </a>`; 
-
+  </a>`;
+  }
 }
-
-}
-  getListings()
-
-
-
+getListings();
